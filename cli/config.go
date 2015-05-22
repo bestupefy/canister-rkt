0420f09bbac1211b6101917b02eceee3f4daefdb
Ramit Surana <ramitsurana@gmail.com>
2015-05-22 12:48:17 +0530

    Create config.go

---
+++ cli/config.go
@@ -0,0 +1,14 @@
+package canister-rkt
+
+import (
+	"github.com/citadel/citadel"
+)
+
+type (
+	Engine struct {
+		SSLCertificate string          `json:"ssl-cert,omitempty"`
+		SSLKey         string          `json:"ssl-key,omitempty"`
+		CACertificate  string          `json:"ca-cert,omitempty"`
+		Engine         *citadel.Engine `json:"engine,omitempty"`
+	}
+)
